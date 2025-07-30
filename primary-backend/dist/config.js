c20b9abf475b0fa09e6ca179035e5af01934f2df
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-07-31 01:59:31 +0530

    removed config.js

--- primary-backend/dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SECRET_ACCESS_KEY = exports.ACCESS_KEY_ID = exports.JWT_SECRET = void 0;
-exports.JWT_SECRET = "my_token_secret";
-exports.ACCESS_KEY_ID = "AKIAXKPUZQOR7JGWPB7X";
-exports.SECRET_ACCESS_KEY = "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";
